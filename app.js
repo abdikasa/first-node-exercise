0961c4183c91a033ee59b01e80b1561d879b163a
abdikasa <abdikasa@sheridancollege.ca>
2021-01-20 14:04:45 -0500

    Added basic midleware, express, different routes

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const http = require("http");
-const server = http.createServer();
+const express = require("express");
+const bodyParser = require("body-parser");
 
-server.listen(3000);
+const app = express();
+
+//valid middleware function
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/shop");
+
+//app can serve as a valid request handler.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRoutes);
+app.use(userRoutes);
+
+app.listen(3000);
