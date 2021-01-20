f51363949e4da46e7026af579c53948fc2a83dec
abdikasa <abdikasa@sheridancollege.ca>
2021-01-20 18:11:41 -0500

    added some css, separated view from business, served HTML

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 //valid middleware function
 const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/shop");
+const errorRoutes = require("./routes/errors");
 
 //app can serve as a valid request handler.
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
-app.use(userRoutes);
 
-//send 404; create a catch-all
-app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
-});
+//prefixes routes with admin
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
+app.use(errorRoutes);
 
 app.listen(3000);
