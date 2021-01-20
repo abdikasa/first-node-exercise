d88df77ed7e6fd9b80ec242af29a705f91b2524c
abdikasa <abdikasa@sheridancollege.ca>
2021-01-20 14:15:45 -0500

    Add our first 404 error

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(userRoutes);
 
+//send 404; create a catch-all
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000);
