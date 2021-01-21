58dbd4401dbf610fc9c3504264efabd501cc75c7
abdikasa <abdikasa@sheridancollege.ca>
2021-01-20 20:39:31 -0500

    Added conditional dynamic content using template pug.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "pug");
+//app.set("views", "views");
 
 //valid middleware function
 const adminRoutes = require("./routes/admin");
@@ -9,9 +13,10 @@ const errorRoutes = require("./routes/errors");
 
 //app can serve as a valid request handler.
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 //prefixes routes with admin
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.routes);
 app.use(userRoutes);
 app.use(errorRoutes);
 
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,13 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const rootDir = require("../util/path");
+const admin = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  //console.log("All Products: ", admin.products);
+  //res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = admin.products;
+  res.render("shop", { products, docTitle: "Homepage" });
 });
 
 module.exports = router;
